7743a563f61868aa0005a8513dfd0ef7a8d9a2f3
Antoine Aflalo <antoineaf@gmail.com>
2012-11-28 10:53:22 +0100

    [INIT] Newspapers

--- src/be/ipl/ai/Newspaper.scala
+++ src/be/ipl/ai/Newspaper.scala
@@ -3,5 +3,15 @@ package be.ipl.ai
 import JaCoP.scala._
 
 object Newspaper extends jacop {
-  def main(args: Array[String]) {}
+  def main(args: Array[String]) {
+    //NewsPapers
+    val FT = IntVar("FT", 1, 4);
+    val Guardian = IntVar("Guardian", 1, 4);
+    val Express = IntVar("Express", 1, 4);
+    val Sun = IntVar("Sun", 1, 4);
+
+    //Data
+    val newspapers = { FT; Guardian; Express; Sun };
+    val durations = { 60; 30; 2; 5 };
+  }
 }
