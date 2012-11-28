91d0412872167a5944de96191e916fa64dc232b8
Antoine Aflalo <antoineaf@gmail.com>
2012-11-28 11:29:56 +0100

    [ADD] Constraints

--- src/be/ipl/ai/Newspaper.scala
+++ src/be/ipl/ai/Newspaper.scala
@@ -4,6 +4,9 @@ import JaCoP.scala._
 
 object Newspaper extends jacop {
   def main(args: Array[String]) {
+
+    //Data
+
     //NewsPapers
     val FT = IntVar("FT", 1, 4);
     val Guardian = IntVar("Guardian", 1, 4);
@@ -11,15 +14,18 @@ object Newspaper extends jacop {
     val Sun = IntVar("Sun", 1, 4);
 
     //Data
-    val newspapers = { FT; Guardian; Express; Sun };
-    val durations = {
+    val newspapers = Array { FT; Guardian; Express; Sun };
+    val durations = Array {
       IntVar("60", 60, 60);
       IntVar("30", 30, 30);
       IntVar("2", 2, 2);
       IntVar("2", 2, 2)
     };
     val one = IntVar("One", 1, 1);
-    val ressources = for (i <- List.range(0, durations.getSize())) yield one;
+    val ressources = for (i <- List.range(0, durations.size)) yield one;
     val limits = IntVar("Algy", 1, 1); //Only Algy as reader
+
+    //Constraints
+    alldifferent(newspapers);
   }
 }
