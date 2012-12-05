97464a07c8eedd326803febf5c6e48837cc944e0
Antoine Aflalo <antoineaf@gmail.com>
2012-12-05 10:16:08 +0100

    [MOD] Put everything together

--- src/be/ipl/ai/Newspaper.scala
+++ src/be/ipl/ai/Newspaper.scala
@@ -9,29 +9,29 @@ object Newspaper extends jacop {
     //Data
 
     //NewsPapers
-    val FT = IntVar("FT", 0, 97);
-    val Guardian = IntVar("Guardian", 0, 97);
-    val Express = IntVar("Express", 0, 97);
-    val Sun = IntVar("Sun", 0, 97);
+    val FT = IntVar("FT", 0, 0);
+    val Guardian = IntVar("Guardian", 0, 0);
+    val Express = IntVar("Express", 0, 0);
+    val Sun = IntVar("Sun", 0, 0);
 
     //Data
     val newspapers = Array[IntVar](FT, Guardian, Express, Sun);
     val durations = Array[IntVar](60, 30, 2, 5);
-    JSSP(newspapers, durations);
-    JSSP(addTime(newspapers,15,97), Array[IntVar](75,3,25,10));
-    JSSP(addTime(newspapers,15,97), Array[IntVar](5,15,10,30));
-    JSSP(addTime(newspapers,30,97), Array[IntVar](90,1,1,1));
-   }
-  
-  def addTime(newspapers : Array[IntVar], shift : Int, max : Int) : Array[IntVar] = {
-    val shifted =  for (i <- List.range(0, newspapers.size)) yield new IntVar(newspapers(i).id,shift,max);
+    val papers = setTasks(newspapers, 0, 97, "Algy") ::: setTasks(newspapers, 15, 97, "Berty") ::: setTasks(newspapers, 15, 97, "Charlie") ::: setTasks(newspapers, 30, 97, "Digby");
+    val dur = durations ::: (Array[IntVar](75, 3, 25, 10)) ::: (Array[IntVar](5, 15, 10, 30)) ::: (Array[IntVar](90, 1, 1, 1));
+    JSSP(papers, dur);
+
+  }
+
+  def setTasks(newspapers: Array[IntVar], shift: Int, max: Int, name: String): Array[IntVar] = {
+    val shifted = for (i <- List.range(0, newspapers.size)) yield new IntVar(name + "_" + newspapers(i).id, shift, max);
     return shifted;
   }
-  
-  def JSSP(newspapers : Array[IntVar] , durations : Array[IntVar]) : Boolean = {
+
+  def JSSP(newspapers: Array[IntVar], durations: Array[IntVar]): Boolean = {
     val one = IntVar("One", 1, 1);
     val ressources = for (i <- List.range(0, durations.size)) yield one;
-    val limit = IntVar("limit", 1, 1); 
+    val limit = IntVar("limit", 1, 1);
 
     //Constraints
     alldistinct(newspapers);
