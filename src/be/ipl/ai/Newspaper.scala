088f2a440a134d9ec521e624cf339ad31523eaa1
Antoine Aflalo <antoineaf@gmail.com>
2012-12-12 12:01:14 +0100

    Display results

--- src/be/ipl/ai/Newspaper.scala
+++ src/be/ipl/ai/Newspaper.scala
@@ -8,22 +8,39 @@ object Newspaper extends jacop {
   def main(args: Array[String]) {
 
     val durations = List[List[Int]](List[Int](60, 30, 2, 5), List[Int](25, 75, 3, 10), List[Int](10, 15, 5, 30), List[Int](1, 1, 1, 90));
-    newspaperProblem(List[String]("Algy", "Berty", "Charlie", "Digby"), List[Int](0, 15, 15, 60), List[String]("Financial Times", "Guardian", "Express", "Sun"), durations)
+    val names = List[String]("Algy", "Berty", "Charlie", "Digby");
+    val result = newspaperProblem(names, List[Int](0, 15, 15, 60), List[String]("Financial Times", "Guardian", "Express", "Sun"), durations)
+    printResult(result _1, result _2, result _3, names)
   }
-  def newspaperProblem(readers: List[String], wakeUpTimes: List[Int], newspapers: List[String], durations: List[List[Int]]): (Int, List[List[IntVar]]) = {
+
+  def printResult(max: Int, result: List[List[IntVar]], endTimes: List[List[IntVar]], names: List[String]) = {
+    println("Durée maximum : " + max + " minutes");
+    for (i <- List.range(0, result.size)) {
+      println(names(i) + " : ")
+      for (j <- List.range(0, result(i).size)) {
+        val newspaper = result(i).sort((a, b) => a.value() < b.value())(j)
+        println("\t \t Commence le " + newspaper.id + " en " + newspaper.value() + " et fini en " + endTimes(i).sort((a, b) => a.value() < b.value())(j).value + " ")
+
+      }
+      println;
+      println
+    }
+  }
+
+  def newspaperProblem(readers: List[String], wakeUpTimes: List[Int], newspapers: List[String], durations: List[List[Int]]): (Int, List[List[IntVar]], List[List[IntVar]]) = {
     val allDurations = for (i <- List.range(0, readers.size)) yield for (j <- List.range(0, durations(i).size)) yield new IntVar("Dur_" + durations(i)(j), durations(i)(j), durations(i)(j))
-    val startTimes = for (i <- List.range(0, readers.size)) yield setTasks(newspapers, wakeUpTimes(i), readers(i), allDurations(i));
+    val startTimes = for (i <- List.range(0, readers.size)) yield setTasks(newspapers, wakeUpTimes(i), allDurations(i));
     val nbNewsPaper = newspapers.size;
     val nbReaders = readers.size;
     val result = JSSP(startTimes, allDurations, nbNewsPaper, nbReaders)
-    var max = result._2.flatten.foldLeft(0)((a, b) => if (a > b.value()) a else b.value())
-    return (max, result._1);
+    val max = result._2.flatten.foldLeft(0)((a, b) => if (a > b.value()) a else b.value())
+    return (max, result._1, result _2);
   }
-  def setTasks(newspapers: List[String], min: Int, name: String, durations: List[IntVar]): List[IntVar] = {
+  def setTasks(newspapers: List[String], min: Int, durations: List[IntVar]): List[IntVar] = {
     val one = IntVar("One", 1, 1);
 
     val max = durations.map(_.value()).foldLeft(0)(_ + _)
-    val tasks = for (i <- List.range(0, newspapers.size)) yield new IntVar(name + "_" + newspapers(i), min, min + max);
+    val tasks = for (i <- List.range(0, newspapers.size)) yield new IntVar(newspapers(i), min, min + max);
     val ressources = for (i <- List.range(0, newspapers.size)) yield one;
     //Cumulatives
     cumulative(tasks, durations.toArray, ressources, one);
