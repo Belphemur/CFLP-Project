8c40b2c0937d3f8182d50f0200aefe9fde76e70d
Antoine Aflalo <antoineaf@gmail.com>
2012-12-12 10:51:44 +0100

    Using Functional programming.

--- src/be/ipl/ai/Newspaper.scala
+++ src/be/ipl/ai/Newspaper.scala
@@ -11,84 +11,70 @@ object Newspaper extends jacop {
     newspaperProblem(List[String]("Algy", "Berty", "Charlie", "Digby"), List[Int](0, 15, 15, 60), List[String]("Financial Times", "Guardian", "Express", "Sun"), durations)
   }
   def newspaperProblem(readers: List[String], wakeUpTimes: List[Int], newspapers: List[String], durations: List[List[Int]]): List[IntVar] = {
-    var startTimes = Array[IntVar]();
-    var allDurations = List[IntVar]();
-    for (i <- 0 to readers.size - 1) {
-      var dur = List[IntVar]();
-      val tempDur = durations(i);
-      for (j <- 0 to tempDur.size - 1) {
-        dur = dur ::: List[IntVar](tempDur(j));
-      }
-      allDurations = allDurations ::: dur;
-      startTimes = startTimes ::: setTasks(newspapers, wakeUpTimes(i), readers(i), dur);
-    }
+    val allDurations = for (i <- 0 to readers.size - 1) yield for (j <- 0 to durations(i).size - 1) yield new IntVar("Dur_" + durations(i)(j), durations(i)(j), durations(i)(j))
+    val startTimes = for (i <- 0 to readers.size - 1) yield setTasks(newspapers, wakeUpTimes(i), readers(i), allDurations(i));
     val nbNewsPaper = newspapers.size;
     val nbReaders = readers.size;
     val result = JSSP(startTimes, allDurations, nbNewsPaper, nbReaders)
-    var max = 0;
-    var generalResultReader = List[IntVar]();
-    for (i <- 0 to result.size - 1) {
-      var perReader = List[IntVar]();
-      val res = result(i);
-      if (res.value > max)
-        max = res.value
-      perReader = perReader ::: List[IntVar](res);
-      if (i + 1 % nbNewsPaper == 0) {
-        generalResultReader = generalResultReader ::: perReader;
-        perReader = List[IntVar]();
-      }
-    }
-
-    return List[IntVar](max) ::: generalResultReader;
+    //    var max = 0;
+    //    var generalResultReader = List[IntVar]();
+    //    for (i <- 0 to result.size - 1) {
+    //      var perReader = List[IntVar]();
+    //      val res = result(i);
+    //      if (res.value > max)
+    //        max = res.value
+    //      perReader = perReader ::: List[IntVar](res);
+    //      if (i + 1 % nbNewsPaper == 0) {
+    //        generalResultReader = generalResultReader ::: perReader;
+    //        perReader = List[IntVar]();
+    //      }
+    //    }
+    //
+    //    return List[IntVar](max) ::: generalResultReader;
+    return List[IntVar]();
   }
-  def setTasks(newspapers: List[String], min: Int, name: String, durations: Array[IntVar]): List[IntVar] = {
+  def setTasks(newspapers: List[String], min: Int, name: String, durations: IndexedSeq[IntVar]): IndexedSeq[IntVar] = {
     val one = IntVar("One", 1, 1);
-    def intVarSum(toSum: Array[IntVar]): Int = {
-      var i = 0
-      var sum = 0;
-      while (i < toSum.length) {
-        sum += toSum(i).value();
-        i += 1
-      }
-      return sum;
-    }
-    val max = intVarSum(durations);
+
+    val max = durations.map(_.value()).foldLeft(0)(_ + _)
     val tasks = for (i <- List.range(0, newspapers.size)) yield new IntVar(name + "_" + newspapers(i), min, min + max);
     val ressources = for (i <- List.range(0, newspapers.size)) yield one;
     //Cumulatives
-    cumulative(tasks, durations, ressources, one);
-    return tasks;
+    cumulative(tasks, durations.toArray, ressources, one);
+    return tasks.toIndexedSeq;
   }
 
-  def JSSP(startTimes: Array[IntVar], durations: Array[IntVar], nbNewsPaper: Int, nbReaders: Int): List[IntVar] = {
+  def JSSP(startTimes: IndexedSeq[IndexedSeq[IntVar]], durations: IndexedSeq[IndexedSeq[IntVar]], nbNewsPaper: Int, nbReaders: Int): IndexedSeq[IndexedSeq[IntVar]] = {
 
+    def concact(toConcact: IndexedSeq[IndexedSeq[IntVar]]): List[IntVar] = {
+      var times = List[IntVar]();
+      for (i <- List.range(0, toConcact.size))
+        for (j <- 0 to toConcact(i).size - 1)
+          times = times ::: List[IntVar](toConcact(i)(j))
+      return times;
+    }
     //endtimes
-    val endTimes = for (i <- List.range(0, durations.size)) yield (startTimes(i) + durations(i));
+    val endTimes = for (i <- List.range(0, startTimes.size)) yield for (j <- 0 to startTimes(i).size - 1) yield (durations(i)(j) + startTimes(i)(j));
     //Constraint
-    for (curNp <- 0 to nbNewsPaper - 1)
-      for (curRd <- 0 to nbReaders - 1) {
-        val curTask = curNp + (curRd * nbReaders);
+    for (curRd <- 0 to nbReaders - 1)
+      for (curNp <- 0 to nbNewsPaper - 1) {
         for (otherRd <- 0 to nbReaders - 1) {
           if (otherRd != curRd) {
-            val otherTask = curNp + (otherRd * nbReaders);
             //println(curTask + " -> " + otherTask)
-            OR(endTimes(curTask) #<= startTimes(otherTask), endTimes(otherTask) #<= startTimes(curTask));
+            OR(endTimes(curRd)(curNp) #<= startTimes(otherRd)(curNp), endTimes(otherRd)(curNp) #<= startTimes(curRd)(curNp));
           }
         }
 
       }
     def printSol(): Unit = {
-      var i = 0;
-      for (v <- startTimes) {
-        print(v.id + " " + v.value)
-        print(" -> " + endTimes(i).value() + " ")
-        i = i + 1;
-        if (i % nbNewsPaper == 0)
-          println();
+      for (curRd <- 0 to nbReaders - 1) {
+        for (curNp <- 0 to nbNewsPaper - 1) {
+          print(startTimes(curRd)(curNp).id + " " + startTimes(curRd)(curNp).value + " -> " + endTimes(curRd)(curNp).value + " ")
+        }
+        println;
       }
-      println()
     }
-    minimize(search(startTimes, first_fail, indomain_middle), max(endTimes));
+    minimize(search(concact(startTimes), first_fail, indomain_middle), max(concact(endTimes.toIndexedSeq)));
     printSol
     return startTimes;
   }
